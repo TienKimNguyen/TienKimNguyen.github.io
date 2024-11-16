32ea0e65419cd59e4c8cb4ead4f11fa53c280d28
TienKimNguyen <ktngn@yahoo.com>
2024-11-16 00:04:16 -0500

    Update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,30 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function toggleMenu() {
+	var menuContainer = document.getElementById("menuContainer");
+	menuContainer.classList.toggle("active"); // Toggle the 'active' class
+  }
+
+ // Close the menu if clicked outside or on a menu item (li)
+ document.addEventListener('click', function(event) {
+	var menuContainer = document.getElementById("menuContainer");
+	var menuIcon = document.querySelector('.menu-icon');
+	var menuItems = document.querySelectorAll('.menu-container li');
+
+	// If the click is outside the menu and the menu is active, close it
+	if (!menuContainer.contains(event.target) && !menuIcon.contains(event.target) && menuContainer.classList.contains("active")) {
+	  menuContainer.classList.remove("active");
+	}
+
+	// Close the menu if any <li> is clicked
+	menuItems.forEach(function(item) {
+	  item.addEventListener('click', function() {
+		menuContainer.classList.remove("active"); // Close the menu when any <li> is clicked
+	  });
+	});
+  });
+
 (function($) {
 
 	var	$window = $(window),
@@ -70,6 +94,9 @@
 								.addClass('active')
 								.addClass('active-locked');
 
+						// Close the menu
+						$('.menu-container').removeClass('active');
+
 					})
 					.each(function() {
 
@@ -100,6 +127,93 @@
 										if ($nav_a.filter('.active-locked').length == 0) {
 
 											$nav_a.removeClass('active');
+											$this.addClass('active');
+
+										}
+
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+
+								}
+							});
+
+					});
+
+		}
+
+		var $nav_mobile = $('.navmobile');
+
+		if ($nav_mobile.length > 0) {
+
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav_mobile.addClass('alt');
+						},
+						leave: function() {
+							$nav_mobile.removeClass('alt');
+						},
+					});
+
+			// Links.
+				var $nav_mobile_a = $nav_mobile.find('a');
+
+				$nav_mobile_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav_mobile.height(); }
+					})
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							$nav_mobile_a
+								.removeClass('active')
+								.removeClass('active-locked');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
+
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
+
+								},
+								enter: function() {
+
+									// Activate section.
+										$section.removeClass('inactive');
+
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($nav_mobile_a.filter('.active-locked').length == 0) {
+
+											$nav_mobile_a.removeClass('active');
 											$this.addClass('active');
 
 										}
